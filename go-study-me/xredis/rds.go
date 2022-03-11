62eb186020d66876fbe8354b3fa5f56c472acc15
panlq <1191243580@qq.com>
2022-03-11 16:05:22 +0800

    feat: 增加hset测试

--- go-study-me/xredis/rds.go
+++ go-study-me/xredis/rds.go
@@ -13,6 +13,7 @@ const testScript = `local ttl_window = tonumber(ARGV[1])
 if redis.call("EXISTS", KEYS[1]) == 1 then
     redis.call("expire", KEYS[1], ttl_window)
 	local eid = redis.call("hget", KEYS[1], "name")
+	redis.call("hset", KEYS[1], "age", 18)
 	return eid
 else
 	return ""
@@ -24,13 +25,13 @@ func main() {
 	db := 0
 	pwd := ""
 	client := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%d", host, port),
-		DB: db,
-		DialTimeout: 5 * time.Second,
-		ReadTimeout: 10 * time.Second,
+		Addr:         fmt.Sprintf("%s:%d", host, port),
+		DB:           db,
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		Password: pwd,
-		PoolSize: 4,
+		Password:     pwd,
+		PoolSize:     4,
 	})
 
 	if _, err := client.Ping().Result(); err != nil {
@@ -38,17 +39,16 @@ func main() {
 	}
 
 	hashMap := map[string]interface{}{
-		"name": "张三",
+		"name":   "张三",
 		"gender": "male",
-		"age": 15,
-		"job": "dba",
+		"age":    15,
+		"job":    "dba",
 	}
-	
+
 	key := "test:lua:script"
 	if _, err := client.HMSet(key, hashMap).Result(); err != nil {
 		log.Fatal(err)
 	}
-
 
 	val, err := client.HGet(key, "name").Result()
 	if err != nil {
@@ -56,6 +56,13 @@ func main() {
 	}
 
 	fmt.Println(val)
+
+	age, err := client.HGet(key, "age").Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v, %T", age, age)
 
 	resp, err := client.Eval(testScript, []string{key}, strconv.Itoa(80)).Result()
 	if err != nil {
